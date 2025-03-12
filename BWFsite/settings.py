859d04d45ea48fba9fa1370f2982a30f37ab82a1
chrizcut <christine_cutting@msn.com>
2025-03-12 11:45:11 +0100

    Added whitenoise for static files

--- BWFsite/settings.py
+++ BWFsite/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",  # Add this line!
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -120,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # Required for Render
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"  # Whitenoise storage for compressed static files
+
 MEDIA_ROOT = os.path.join(BASE_DIR, "")
 MEDIA_URL = "/media/"
 
